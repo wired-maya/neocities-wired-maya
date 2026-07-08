16a8ea32852373e9f15f755aeb6f29e16ab41868
wired-maya <dhodul@gmail.com>
2026-07-07 19:52:26 -0700

    Rewrote gallery

---
+++ js/gallery.js
@@ -0,0 +1,62 @@
+export class Gallery {
+    index = 0;
+    folderPath = "/";
+    imageNames = [];
+
+    slider;
+    img;
+
+    constructor(sliderId, imgId, folderPath, imgArray) {
+        this.slider = document.getElementById(sliderId);
+        this.img = document.getElementById(imgId);
+
+        this.folderPath = folderPath;
+        this.images = imgArray;
+
+        this.slider.max = this.images.length - 1;
+
+        this.setGallery(0);
+    }
+
+    scrollGallery(offset) {
+        this.index += offset; // Can just add here because modulo calc is done later
+
+        this.setGallery(this.index);
+    }
+
+    // Overflow values wrap around
+    setGallery(pos) {
+        this.index = Math.abs((this.images.length + parseInt(pos)) % this.images.length);
+
+        this.slider.value = this.index;
+        this.img.src = this.folderPath + this.images[this.index];
+    }
+}
+
+//===== Gallery instances =====//
+
+const kittySliderElement = "kittySliderElement";
+const kittyImgElement = "kittyImgElement";
+const kittyImgPath = "/assets/gallery/my-cat/";
+const kittyImgNames = [ // No better way to do this client side
+    "PXL_20230215_210425193.jpg",
+    "PXL_20230303_222245983.jpg",
+    "PXL_20230303_222307987.jpg",
+    "PXL_20230425_211841997.jpg",
+    "PXL_20230425_211932289.jpg",
+    "PXL_20230425_212031667.MP.jpg",
+    "PXL_20230601_155407214.jpg",
+    "PXL_20230720_020921585.MP.jpg",
+    "PXL_20230720_020945077.MP.jpg",
+    "PXL_20230722_041157156.MP.jpg",
+    "PXL_20230801_001157041.jpg"
+];
+
+const kittyGallery = new Gallery(
+    kittySliderElement,
+    kittyImgElement,
+    kittyImgPath,
+    kittyImgNames
+);
+
+window.kittyGallery = kittyGallery;
